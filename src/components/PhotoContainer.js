61b792097ffde34daa3636208f5de72b0282a2be
Abigail Barrera <31635503+abigailbarrera@users.noreply.github.com>
2018-03-25 17:02:03 -0700

    Updated PhotoContainer.js

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -25,7 +25,7 @@ class PhotoContainer extends Component {
         });
       })
       .catch(function (error) {
-        <h3>Error fetching and parsing data:{ error }</h3>;
+        alert("Error fetching and parsing data:{ error }");
       });
   }
 
@@ -42,6 +42,6 @@ class PhotoContainer extends Component {
       </div>
     );
   }
-}
+};
 
 export default PhotoContainer;
